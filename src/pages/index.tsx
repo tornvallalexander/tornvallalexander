d433cb85e4097841ee4601654b9f6c4bc1eff32a
Alexander Törnvall <alexander.tornvall@icloud.com>
2021-10-15 22:07:02 +0200

    basic setup contentful article card preview

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,70 @@
 import * as React from "react"
 
 import Layout from "../components/layout"
-import IndexHero from "../components/indexHero"
+import ArticleCard from "../components/ArticleCard";
+import { graphql, useStaticQuery } from "gatsby"
+import { ImageDataLike } from "gatsby-plugin-image"
 
-const IndexPage = () => (
-  <Layout>
-    <IndexHero />
-  </Layout>
-)
+type IndexQueryTypes = {
+  allContentfulBlogPost: {
+    edges: {
+      node: {
+        author: {
+          fullName: string,
+          avatarImage: ImageDataLike,
+        }
+        updatedAt: string,
+        metaDescription: string,
+        title: string,
+        categories: string[],
+        slug: string,
+      }
+    }[]
+  }
+}
+
+const IndexPage = () => {
+  const { allContentfulBlogPost } = useStaticQuery<IndexQueryTypes>(
+    graphql`
+      {
+        allContentfulBlogPost {
+          edges {
+            node {
+              author {
+                fullName
+                avatarImage {
+                  gatsbyImageData(width: 50)
+                }
+              }
+              updatedAt(formatString: "MMMM DD YYYY", locale: "en")
+              metaDescription
+              title
+              categories
+              slug
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <Layout>
+      { allContentfulBlogPost.edges.map((post) => {
+        const { author, updatedAt, metaDescription, title, categories, slug} = post.node
+        return (
+          <ArticleCard
+            author={author}
+            categories={categories}
+            metaDescription={metaDescription}
+            slug={slug}
+            title={title}
+            updatedAt={updatedAt}
+          />
+        )
+      })}
+    </Layout>
+  )
+}
 
 export default IndexPage
